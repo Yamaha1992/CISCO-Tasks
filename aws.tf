4e2ece1bb060a0507eda5a4ead1220ab94c810d3
Yamaha1992 <38129822+Yamaha1992@users.noreply.github.com>
2020-05-12 18:53:32 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAXC5OTRPNUFBAZL3P"
-  secret_key = "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"
+  access_key = "*****************"
+  secret_key = "*************************"
 }
